d764c2ddea3639056a10b3648b0fa8f479b4dfe3
stansmith907 <stansmith@usgs.gov>
2014-11-06 16:47:33 -0900

    - added factSheet to scope - bumped version to 0.1.1

--- lib/adiwg/mdcodes/version.rb
+++ lib/adiwg/mdcodes/version.rb
@@ -2,13 +2,14 @@
 
 # version history
 
-# 0.1.0 2014-11-07 first release
+# 0.1.0 2014-11-05 first release
+# 0.1.1 2014-11-06 added factSheet to scope
 #
 
 module ADIWG
 
 	module Mdcodes
-		VERSION = "0.1.0"
+		VERSION = "0.1.1"
 	end
 
 end
